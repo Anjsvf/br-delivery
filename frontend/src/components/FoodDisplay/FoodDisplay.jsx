013e879fc531f5f1e492ea7225f3d4def44887c4
antonio santos <antoniosantos.devweb@gmail.com>
2024-07-14 14:49:01 -0300

    log delete on foodDispaly

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,7 +9,7 @@ const FoodDisplay = ({ category }) => {
       <h2>pratos principais pertinho de você</h2>
       <div className="food-display-list">
         {food_list.map((item,index) => {
-          {console.log(category,item.category);}
+          // {console.log(category,item.category);}
           if (category==="All" || category===item.category) {
             return (
 
