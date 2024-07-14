746a6f399cd537957289e20e81f409b531a45f2f
antonio santos <antoniosantos.devweb@gmail.com>
2024-07-14 18:01:46 -0300

    link add

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://br-delivery-frontend.onrender.com";
+  const frontend_url = "https://br-delivery-28yw-frontend.vercel.app/";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
